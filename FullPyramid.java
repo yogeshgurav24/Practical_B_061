6579c779e90c1c69cf163b1e08ccd5bf0de86ade
Anas Ansari <anasff5135@gmail.com>
2026-02-22 23:38:43 +0530

    Practical 4 A Done By Anas Ansari 064

--- FullPyramid.java
+++ FullPyramid.java
@@ -1,13 +1,20 @@
-public class FullPyramid {
-
+public class AlphabetFullPyramid {
     public static void main(String[] args) {
 
         int rows = 5;
 
-        for (int i = rows; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+        for (int i = 1; i <= rows; i++) {
+
+            // Print spaces for center alignment
+            for (int space = 1; space <= rows - i; space++) {
+                System.out.print(" ");
             }
+
+            // Print alphabets
+            for (int j = 0; j < i; j++) {
+                System.out.print((char)('A' + j) + " ");
+            }
+
             System.out.println();
         }
     }
--- HalfPyramid.java
+++ HalfPyramid.java
@@ -1,11 +1,16 @@
-public class HalfPyramid {
-     public static void main(String[] args) {
+public class NumberInvertedPattern {
+    public static void main(String[] args) {
 
-        int rows = 6;
+        int num = 1;
 
-        for (int i = 1; i <= rows; i++) {
+        for (int i = 5; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
+                System.out.print(num + " ");
+                num++;
+
+                if (num > 9) {
+                    num = 1;
+                }
             }
             System.out.println();
         }
